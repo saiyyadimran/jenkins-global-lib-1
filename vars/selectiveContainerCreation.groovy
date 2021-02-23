9c3c99b99fd43e6285b7e47f2aaf35c658c2c779
jojanmathew <55776892+jojanmathew@users.noreply.github.com>
2021-02-22 23:00:20 -0500

    Add files via upload

---
+++ vars/selectiveContainerCreation.groovy
@@ -0,0 +1,25 @@
+podTemplate(containers: [
+    containerTemplate(name: 'java', image: 'openjdk:17-ea-10-jdk', ttyEnabled: true, command: 'cat'),
+    containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
+  ]) {
+
+    node(POD_LABEL) {
+        stage('Get a Java project') {
+            git 'https://github.com/jenkinsci/kubernetes-plugin.git'
+            container('java') {
+                stage('Check java version') {
+                    sh 'java -version'
+                }
+            }
+        }
+        stage('Get a Maven project') {
+            git 'https://github.com/jenkinsci/kubernetes-plugin.git'
+            container('maven') {
+                stage('Build a Maven project') {
+                    sh 'mvn -B clean install'
+                }
+            }
+        }
+
+    }
+}
