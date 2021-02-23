cbe967973f4d6db5af719f29fea3bd69ec2b080d
saiyyadimran <imran.mitm@gmail.com>
2021-02-24 01:00:01 +0530

    Update mvnCleanInstallWithoutProfile.groovy

--- vars/mvnCleanInstallWithoutProfile.groovy
+++ vars/mvnCleanInstallWithoutProfile.groovy
@@ -1,5 +1,5 @@
 def call(Map args) {
 	def mvnHome = tool 'Maven'
-	sh "cd ${WORKSPACE}"
+	//sh "cd ${WORKSPACE}"
 	sh "'${mvnHome}/bin/mvn' -Dproject.build.sourceEncoding=UTF-8 -Dproject.reporting.outputEncoding=UTF-8 -Dmaven.test.skip=${args.skipTest} clean install"
 }
