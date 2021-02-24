5550694c287866785315e6efdcdc8028d8ddb7b3
saiyyadimran <imran.mitm@gmail.com>
2021-02-24 21:23:10 +0530

    Update selectiveContainerCreationLatest.groovy

--- vars/selectiveContainerCreationLatest.groovy
+++ vars/selectiveContainerCreationLatest.groovy
@@ -1,5 +1,5 @@
 def call(Map args) {
-  podTemplateLatest(containers: [
+  podTemplate(containers: [
     containerTemplate(name: 'java', image: 'openjdk:17-ea-10-jdk', ttyEnabled: true, command: 'cat'),
     containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
   ]) {
